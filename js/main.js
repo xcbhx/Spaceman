ddf43ddc69cbfcfe227a6325078fc0e6dc660048
xcbhx <ceina.ellison@gmail.com>
2022-10-09 16:48:31 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 /*----- constants -----*/
-max wrong guesses
-words = [
-    'software Engineer',
-    'Funeral Director',
-    '',
-    '',
-    '',
-]
+// max wrong guesses
+// words = [
+//     'software Engineer',
+//     'Funeral Director',
+//     '',
+//     '',
+//     '',
+// ]
 
 /*----- app's state (variables) -----*/
-let board; // array of 3 (horizontal) row 
-let turn, // 
-let winner; // null = no winner; 1 or -1 winner
+// let board; // array of 3 (horizontal) row 
+// let turn, // 
+// let winner; // null = no winner; 1 or -1 winner
 
 /*----- cached element references -----*/
 
