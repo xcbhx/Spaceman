9774c07565c95be6259d1a276608cbe0f3704de6
xcbhx <ceina.ellison@gmail.com>
2022-10-10 20:54:23 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*----- constants -----*/
-// max wrong guesses
+const maxWrong =  6; 
+
 const WORDS = [
     'Software Engineer',
     'Funeral Director',
@@ -7,6 +8,15 @@ const WORDS = [
     'President',
     'Photographer',
 ];
+
+// const letterBank = [
+//     'A', 'B', 'C', 'D', 'E', 'F', 'G','H', 'I',
+//     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+//     'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+// ]
+
+// const wrongSe = WORDS[0];
+
 
 let IMAGES = [
     "img/spaceman-filmstrip.png/spaceman-0.jpg",
@@ -16,59 +26,60 @@ let IMAGES = [
     "img/spaceman-filmstrip.png/spaceman-4.jpg", 
     "img/spaceman-filmstrip.png/spaceman-5.jpg", 
     "img/spaceman-filmstrip.png/spaceman-6.jpg",
-]
+];
 
 /*----- app's state (variables) -----*/
 let randomWord; // array of 3 (horizontal) row 
 // let turn,
-let winner; // null = no winner; 
-let guesses;
+let gameStatus; // null = no winner; W = win; L = lose
+let guess;
+let wrongGuesses;
+
+
 /*----- cached element references -----*/
 // const playAgainBtn = document.querySelector('button');
 // const boardLetters = [...document.querySelctorAll('#board > button')];
-const imageEl = document.getElementById('spaceman');
+const imageEl = document.querySelector('img');
 const wordEl = document.getElementById('wordGuess');
+const messageEl = document.getElementById('message');
+const plyBtn = document.getElementById('playAgain');
+const letterButton = [...document.querySelectorAll('section > button')]
 
 // const imgPath = `imgs/spaceman-0${wrongGuesses.length}`;
 
 /*----- event listeners -----*/
-document.getElementById('letters').addEventListener('click', handleChoice);
-// playAgainBtn.addEventListener('click', initialize);
+document.querySelector('section').addEventListener('click', handleChoice);
+plyBtn.addEventListener('click', init);
+
+
 
 /*----- functions -----*/
 init ();
 
 
 function init() {
+    wrongGuesses = [];
     let rndIdx = Math.floor(Math.random() * WORDS.length)
-    randomWord = WORDS[rndIdx].split('');
+    randomWord = WORDS[rndIdx].toUpperCase().split('');
     // console.log(randomWord);
-    guesses = 0;
-    winner = null;
-    randomWord.forEach(function () {
-        wordEl.appendChild(document.createElement('div'))
-    })
+    guess = randomWord.map(ltr => ltr === ' ' ? ' ' : '_');
+    gameStatus = null;
     render(); //invoke render 
 }
 
 function render() {
-    imageEl.src = IMAGES[guesses];
-    let chars = [...document.querySelectorAll('#wordGuess > div')];
-    console.log(wordEl, chars);
-    randomWord.forEach(function (letter, idx) {
-        chars[idx].innerHTML = ' '; 
-        // chars[idx].innerHTML = letter; 
-    })
-    console.log([...document.querySelectorAll('#wordGuess > div')]);
+    imageEl.src = `${IMAGES[wrongGuesses.length]}`;
+    wordEl.textContent = guess.join('');
 }
+
 
 function handleChoice(evt) {
     if (evt.target.tagName !== 'BUTTON') return;
-
-
     guesses += 1; 
     render();
 }
+
+
 
 // const MAX_LETTER = parseInt();
 // const MAX_Wrong = 6; 
